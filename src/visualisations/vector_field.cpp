992de55d2693953b03359b078f93ef62d06a9906
Martin Prazak <martin.prazak@gmail.com>
2016-03-12 18:14:08 +0000

    Vector field visualisation lines now have arrows

--- src/visualisations/vector_field.cpp
+++ src/visualisations/vector_field.cpp
@@ -24,20 +24,28 @@ vector_field::~vector_field() {
 void vector_field::draw(std::ostream& svg, const agents& input, const scenarios::base& _scenario) const {
 	const scenarios::shepards& scenario = dynamic_cast<const scenarios::shepards&>(_scenario);
 
+	svg << "<defs>"
+    			"<marker id=\"field_arrow\" markerWidth=\"5\" markerHeight=\"5\" refX=\"1.5\" refY=\"1.5\" orient=\"auto\" markerUnits=\"strokeWidth\">"
+      				"<path d=\"M0,0 L0,3 L4.5,1.5 z\" style=\"" << m_style << "\" />"
+    			"</marker>"
+  			"</defs>" << std::endl;
+
 	const unsigned x_sample_count = round((m_bbox.size().x - (float)m_sampling * 0.2f) / (float)m_sampling);
 	const unsigned y_sample_count = round((m_bbox.size().y - (float)m_sampling * 0.2f) / (float)m_sampling);
 
 	for(unsigned xi=0; xi<x_sample_count; ++xi)
 		for(unsigned yi=0; yi<y_sample_count; ++yi) {
 			const Imath::V2f pos(xi * m_sampling + m_sampling/2, yi * m_sampling + m_sampling/2);
-			const Imath::V2f val = scenario.sample(pos) * (float)m_sampling/2.0f;
+			const Imath::V2f val = scenario.sample(pos) * (float)m_sampling * 0.4f;
 
 			svg << "<line "
 				"x1=\"" << (pos.x - val.x) << "\" "
 				"y1=\"" << (pos.y - val.y) << "\" "
 				"x2=\"" << (pos.x + val.x) << "\" "
 				"y2=\"" << (pos.y + val.y) << "\" "
-				"style=\"" << m_style << "\" />" << std::endl;
+				"style=\"" << m_style << "\" "
+				"marker-end=\"url(#field_arrow)\" "
+				" />" << std::endl;
 		}
 }
 
