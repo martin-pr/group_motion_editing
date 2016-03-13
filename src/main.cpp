cd6a52b5b5fb68d829b03477751c8fe366b034bf
Martin Prazak <martin.prazak@gmail.com>
2016-03-13 23:33:39 +0000

    Each scenario can now contain multiple setups, reusing the same visualisations

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,17 @@ namespace po = boost::program_options;
 using std::cout;
 using std::endl;
 
+namespace {
+	struct scenario_setup {
+		scenario_setup(const std::string& n) : name(n) {
+		}
+
+		std::string name;
+		std::vector<std::pair<std::string, std::unique_ptr<scenarios::base>>> scenarios;
+		std::vector<std::pair<std::string, std::unique_ptr<visualisation>>> visualisations;
+	};
+}
+
 int main(int argc, char* argv[]) {
 	// Declare the supported options.
 	po::options_description desc("Allowed options");
@@ -43,37 +54,50 @@ int main(int argc, char* argv[]) {
 	// cout << tree << endl;
 
 	// read the agent setups
-	std::map<std::string, agent_setup> agent_setups;
+	std::vector<std::pair<std::string, agent_setup>> agent_setups;
 	for(auto& setup : tree.get_child("agent_setups"))
-		agent_setups.insert(std::make_pair(setup.first, agent_setup(setup.second)));
+		agent_setups.push_back(std::make_pair(setup.first, agent_setup(setup.second)));
 
 	// read the scenarios
-	std::map<std::string, std::unique_ptr<scenarios::base>> scenarios;
-	for(auto& setup : tree.get_child("scenarios"))
-		scenarios.insert(std::make_pair(setup.first,
-			std::move(factory<scenarios::base, boost::property_tree::ptree>::singleton().create(
-				setup.second.get_child("type").data(),
-				setup.second))));
+	std::vector<scenario_setup> scenario_setups;
+	for(auto& scen : tree.get_child("scenarios")) {
+		scenario_setups.push_back(scenario_setup(scen.first));
+
+		for(auto& setup : scen.second.get_child("setups")) {
+			scenario_setups.back().scenarios.push_back(std::make_pair(setup.first,
+				std::move(factory<scenarios::base, boost::property_tree::ptree>::singleton().create(
+					scen.second.get_child("type").data(),
+					setup.second))));
+		}
+
+		for(auto& vis : scen.second.get_child("visualisations")) {
+			scenario_setups.back().visualisations.push_back(std::make_pair(
+				vis.first,
+				std::unique_ptr<visualisation>(new visualisation(vis.second))))	;
+		}
+	}
 
 	// for each scenario, each agent setup and each visualisation, generate an SVG
-	for(auto& s : scenarios) {
+	for(auto& s : scenario_setups) {
 		for(auto& a : agent_setups) {
-			const agents output = s.second->apply(*a.second);
+			for(auto& sc : s.scenarios) {
+				const agents output = sc.second->apply(*a.second);
 
-			for(auto& v : s.second->visualisations()) {
-				const std::string filename = vm["output"].as<std::string>() + "/" + s.first + "_" + a.first + "_" + v.first + ".svg";
+				for(auto& v : s.visualisations) {
+					const std::string filename = vm["output"].as<std::string>() + "/" + s.name + "_" + sc.first + "_" + a.first + "_" + v.first + ".svg";
 
-				std::stringstream svgContent;
-				v.second->draw(svgContent, *a.second, output, *s.second);
+					std::stringstream svgContent;
+					v.second->draw(svgContent, *a.second, output, *sc.second);
 
-				std::ofstream svg(filename.c_str());
+					std::ofstream svg(filename.c_str());
 
-				svg << "<?xml version=\"1.0\" standalone=\"no\"?>" << endl;
-				svg << "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">" << endl;
+					svg << "<?xml version=\"1.0\" standalone=\"no\"?>" << endl;
+					svg << "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">" << endl;
 
-				svg << "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">" << endl;
-				svg << svgContent.str();
-				svg << "</svg>" << endl;
+					svg << "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">" << endl;
+					svg << svgContent.str();
+					svg << "</svg>" << endl;
+				}
 			}
 		}
 	}
--- src/scenarios/base.cpp
+++ src/scenarios/base.cpp
@@ -8,10 +8,10 @@ using std::endl;
 namespace scenarios {
 
 base::base(const boost::property_tree::ptree& config) {
-	for(auto& vis : config.get_child("visualisations"))
-		m_visualisations.insert(std::make_pair(
-			vis.first,
-			std::unique_ptr<visualisation>(new visualisation(vis.second))));
+	// for(auto& vis : config.get_child("visualisations"))
+	// 	m_visualisations.insert(std::make_pair(
+	// 		vis.first,
+	// 		std::unique_ptr<visualisation>(new visualisation(vis.second))));
 }
 
 const std::map<std::string, std::unique_ptr<visualisation>>& base::visualisations() const {
