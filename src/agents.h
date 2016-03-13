7744efdc1802638efb4054e8fef38d79119fad02
Martin Prazak <martin.prazak@gmail.com>
2016-03-13 15:13:41 +0000

    Added heading() method to agents class, allowing to determine overall average heading of a group of agents (moved from heading_direction visualisation)

--- src/agents.h
+++ src/agents.h
@@ -4,6 +4,8 @@
 
 #include <ImathVec.h>
 
+#include "line_fit.h"
+
 class agents {
 	public:
 		struct agent_frame {
@@ -19,6 +21,8 @@ class agents {
 				const_iterator begin() const;
 				const_iterator end() const;
 
+				std::size_t size() const;
+
 			private:
 				agent(unsigned frameCount);
 
@@ -33,6 +37,11 @@ class agents {
 		bool empty() const;
 		unsigned agent_count() const;
 		unsigned frame_count() const;
+
+		/// computes the heading direction and origin (fits a line into the data,
+		///   with start based on lowest edge point projection, and length based on
+		///   the difference between projections of edge frames)
+		line heading() const;
 
 		agent& operator[](unsigned agentId);
 		const agent& operator[](unsigned agentId) const;
--- src/visualisations/heading_direction.cpp
+++ src/visualisations/heading_direction.cpp
@@ -16,36 +16,7 @@ heading_direction::~heading_direction() {
 }
 
 void heading_direction::draw(std::ostream& svg, const agents& input, const agents& output, const scenarios::base& _scenario) const {
-	assert(input.agent_count() > 0 && input.frame_count() > 0);
-
-	// collect all points
-	std::vector<Imath::Vec2<float>> points;
-	for(auto& a : input)
-		for(auto& f : a)
-			points.push_back(f.position);
-
-	// fit the line into points
-	line l = fit(points);
-	l.direction.normalize();
-
-	// make sure it aims the right way, based on the average direction of agents in the first frame
-	{
-		Imath::Vec2<float> firstFrameDir;
-		for(auto& a : input)
-			firstFrameDir += a.begin()->direction.normalized();
-
-		if(l.direction.dot(firstFrameDir) < 0.0f)
-			l.direction = -l.direction;
-	}
-
-	// figure out minimum and maximum parameter (t)
-	float minT = 0, maxT = 0;
-	for(auto& a : input)
-		for(auto& f : a) {
-			const float t = l.direction.dot(f.position - l.origin); // l.direction is normalized
-			minT = std::min(t, minT);
-			maxT = std::max(t, maxT);
-		}
+	const line l = input.heading();
 
 	svg << "<defs>"
     			"<marker id=\"heading_arrow\" markerWidth=\"10\" markerHeight=\"10\" refX=\"3\" refY=\"3\" orient=\"auto\" markerUnits=\"strokeWidth\">"
@@ -54,10 +25,10 @@ void heading_direction::draw(std::ostream& svg, const agents& input, const agent
   			"</defs>" << std::endl;
 
 	svg << "<line "
-		"x1=\"" << (l.origin + l.direction * minT).x << "\" "
-		"y1=\"" << (l.origin + l.direction * minT).y << "\" "
-		"x2=\"" << (l.origin + l.direction * maxT).x << "\" "
-		"y2=\"" << (l.origin + l.direction * maxT).y << "\" "
+		"x1=\"" << (l.origin).x << "\" "
+		"y1=\"" << (l.origin).y << "\" "
+		"x2=\"" << (l.origin + l.direction).x << "\" "
+		"y2=\"" << (l.origin + l.direction).y << "\" "
 		"style=\"" << m_style << "\" "
 		"marker-end=\"url(#heading_arrow)\" "
 		" />" << std::endl;
