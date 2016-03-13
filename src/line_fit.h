bffcf7001e92cc502a24e6caec1f3c85c24fd983
Martin Prazak <martin.prazak@gmail.com>
2016-03-13 15:09:33 +0000

    Line now has correct construction (Imath::Vec2 is not initialised by default)

--- src/line_fit.h
+++ src/line_fit.h
@@ -5,7 +5,8 @@
 #include <ImathVec.h>
 
 struct line {
-	Imath::Vec2<float> origin, direction;
+	Imath::Vec2<float> origin = Imath::Vec2<float>(0,0);
+	Imath::Vec2<float> direction = Imath::Vec2<float>(0,0);
 };
 
 line fit(const std::vector<Imath::Vec2<float>>& points);
