b92471e43afced5b24bad5804e09794351561d77
Martin Prazak <martin.prazak@gmail.com>
2016-03-26 20:41:01 +0000

    Added non-local reference support (reading other json files via $ref) - http://json-spec.readthedocs.org/en/latest/reference.html

--- src/json.cpp
+++ src/json.cpp
@@ -1,6 +1,7 @@
 #include "json.h"
 
 #include <iostream>
+#include <unordered_map>
 
 #include <boost/property_tree/json_parser.hpp>
 
@@ -10,7 +11,7 @@ using std::cout;
 using std::endl;
 
 namespace {
-	// visitor for all references
+	// visitor for all references ($ref child keys)
 	void visitRefs(boost::property_tree::ptree& tree, std::function<void(boost::property_tree::ptree& current, const boost::property_tree::ptree& parent)> visitor) {
 		for(auto& c : tree) {
 			// references have $ref as key, and no children
@@ -30,13 +31,17 @@ boost::property_tree::ptree read_json(const boost::filesystem::path& p) {
 	// read the original content
 	boost::property_tree::read_json(p.string(), tree);
 
-	// find all $refs
-	visitRefs(tree, [&tree](boost::property_tree::ptree& ref, const boost::property_tree::ptree& parent) {
+	// for references support, we might need to read other files
+	//   -> lets cache them, to make sure we don't parse each file more than once
+	std::unordered_map<std::string, boost::property_tree::ptree> cache;
+
+	// find all $refs, in accordance to http://json-spec.readthedocs.org/en/latest/reference.html
+	visitRefs(tree, [&tree, &p, &cache](boost::property_tree::ptree& ref, const boost::property_tree::ptree& parent) {
 		const std::string path = ref.get_child("$ref").data();
 
 		// empty references not allowed
 		if(path.length() < 2)
-			throw std::runtime_error("found empty JSON reference");
+			throw std::runtime_error("found an empty JSON reference");
 		// if data start with #, they're local references
 		else if(path[0] == '#') {
 
@@ -47,8 +52,29 @@ boost::property_tree::ptree read_json(const boost::filesystem::path& p) {
 			else
 				ref = parent.get_child(boost::property_tree::path(path.substr(1), '/'));
 		}
-		else
-			throw std::runtime_error("multi-file references not allowed (yet)");
+		else {
+			auto pos = path.find('#');
+			if(pos == std::string::npos)
+				throw std::runtime_error("no # symbol in a reference");
+			if(pos >= path.length()-1)
+				throw std::runtime_error("no path after # symbol in a reference");
+
+			// make a canonical path
+			boost::filesystem::path can = boost::filesystem::canonical(path.substr(0, pos), p.parent_path());
+			auto it = cache.find(can.string());
+			if(it == cache.end()) {
+				it = cache.insert(std::make_pair(can.string(), boost::property_tree::ptree())).first;
+				boost::property_tree::read_json(can.string(), it->second);
+			}
+			assert(it != cache.end());
+
+			// root path
+			if(path[pos+1] == '/')
+				ref = it->second.get_child(boost::property_tree::path(path.substr(pos+2), '/'));
+			// relative path
+			else
+				throw std::runtime_error("relative references for external files not allowed");
+		}
 	});
 
 	return tree;
